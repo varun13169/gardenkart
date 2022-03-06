fdde8b641aaa8c5cc249b169508c58ebbadf02a5
Varun Kumar <varun@Varun-MacBook-Pro.local>
2022-03-07 01:41:36 +0530

    CartPage.jsx: Change class to className

--- src/pages/cartPage/CartPage.jsx
+++ src/pages/cartPage/CartPage.jsx
@@ -4,21 +4,21 @@ import "./cart-page.css";
 export default function CartPage() {
     return (
         
-    <section class="page-wrap">
-    <section class="page-nav">
+    <section className="page-wrap">
+    <section className="page-nav">
         <Navbar></Navbar>
     </section>
 
-    <section class="page-sidebar">
+    <section className="page-sidebar">
 
     </section>
 
-    <section class="page-main">
-        <main class="main-content">
-            <h3 class="dui-util-spc-mgn-2_4rem-m dui-util-fw-bld">My Cart (2)</h3>
+    <section className="page-main">
+        <main className="main-content">
+            <h3 className="dui-util-spc-mgn-2_4rem-m dui-util-fw-bld">My Cart (2)</h3>
 
-            <div class="cart-container">
-                <div class="cart-container__cart-items ">
+            <div className="cart-container">
+                <div className="cart-container__cart-items ">
                 {[1, 2].map((itemDetails) => {
                     return (
                     <Card/>
@@ -26,32 +26,32 @@ export default function CartPage() {
                 })}
                 </div>
 
-                <div class="cart-container__price-details">
+                <div className="cart-container__price-details">
 
-                    <div class="dui-cart-details-card">
-                        <h3 class="dui-util-fw-bld">PRICE DETAILS</h3>
+                    <div className="dui-cart-details-card">
+                        <h3 className="dui-util-fw-bld">PRICE DETAILS</h3>
                         <hr/>
-                        <div class="dui-util-disp-flex-row">
+                        <div className="dui-util-disp-flex-row">
                             <p>Price (2 items)</p>
-                            <p class="mgn-left-auto">$4999</p>
+                            <p className="mgn-left-auto">$4999</p>
                         </div>
-                        <div class="dui-util-disp-flex-row">
+                        <div className="dui-util-disp-flex-row">
                             <p>Discount</p>
-                            <p class="mgn-left-auto">-$1999</p>
+                            <p className="mgn-left-auto">-$1999</p>
                         </div>
-                        <div class="dui-util-disp-flex-row">
+                        <div className="dui-util-disp-flex-row">
                             <p>Delivery Charges</p>
-                            <p class="mgn-left-auto">$499</p>
+                            <p className="mgn-left-auto">$499</p>
                         </div>
                         <hr/>
-                        <div class="dui-util-disp-flex-row dui-util-fw-bld">
+                        <div className="dui-util-disp-flex-row dui-util-fw-bld">
                             <p>TOTAL AMOUNT</p>
-                            <p class="mgn-left-auto">$3499</p>
+                            <p className="mgn-left-auto">$3499</p>
                         </div>
                         <hr/>
                         <p>You will save $1999 on this order</p>
 
-                        <a class="dui-link dui-link--primary dui-util-txt-sm dui-util-spc-pad-0_8rem-xs dui-util-fw-sbld dui-util-txt-align-cent"
+                        <a className="dui-link dui-link--primary dui-util-txt-sm dui-util-spc-pad-0_8rem-xs dui-util-fw-sbld dui-util-txt-align-cent"
                             href="#">
                             PLACE ORDER
                         </a>
@@ -61,8 +61,8 @@ export default function CartPage() {
         </main>
     </section>
 
-    <section class="page-footer">
-        <footer class="footer">
+    <section className="page-footer">
+        <footer className="footer">
             Some Footer Information
         </footer>
     </section>
