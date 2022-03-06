d6b9f8c7bfb0ccb63d3aa0b2bc0f3f0e72dd9bad
Varun Kumar <varun@Varun-MacBook-Pro.local>
2022-03-07 01:33:51 +0530

    Change class to className

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,17 @@
 export default function Navbar() {
     return(
-        <nav class="dui-nav-sch-act">
-                <a class="dui-nav-sch-act__logo dui-util-txt-decoration-none" href="/pages/homePage/homePage.html">
-                    <h3 class="dui-util-fw-bld dui-primary-color">
+        <nav className="dui-nav-sch-act">
+                <a className="dui-nav-sch-act__logo dui-util-txt-decoration-none" href="/pages/homePage/homePage.html">
+                    <h3 className="dui-util-fw-bld dui-primary-color">
                         Garden Kart
                     </h3>
                 </a>
 
                 {/* <!-- Search --> */}
-                <div class="dui-nav-sch-act__search dui-inp-txt dui-inp-txt--search">
-                    <i class="dui-inp-txt__icon dui-inp-txt__icon--search">
+                <div className="dui-nav-sch-act__search dui-inp-txt dui-inp-txt--search">
+                    <i className="dui-inp-txt__icon dui-inp-txt__icon--search">
                         <svg xmlns="http://www.w3.org/2000/svg"
-                            class="dui-inp-txt__icon-content dui-inp-txt__icon-content--search icon icon-tabler icon-tabler-search"
+                            className="dui-inp-txt__icon-content dui-inp-txt__icon-content--search icon icon-tabler icon-tabler-search"
                             width="56" height="56" viewBox="0 0 24 24" stroke-width="1.5" stroke="#059669" fill="none"
                             stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -21,22 +21,22 @@ export default function Navbar() {
                     </i>
                     <label>
                         <input
-                            class="dui-inp-txt__input dui-inp-txt__input--search dui-util-txt-sm dui-util-bdr-radi-5px-s reset-input-inherit-parent"
+                            className="dui-inp-txt__input dui-inp-txt__input--search dui-util-txt-sm dui-util-bdr-radi-5px-s reset-input-inherit-parent"
                             type="text" placeholder="Search"/>
                     </label>
                 </div>
 
-                <ul class="dui-nav-sch-act__actions dui-ul">
+                <ul className="dui-nav-sch-act__actions dui-ul">
                     <li>
-                        <div class="dui-nav-sch-act__auth-actions">
+                        <div className="dui-nav-sch-act__auth-actions">
                             {/* <!-- SignUp Button --> */}
-                            <a class="dui-nav-sch-act__signup-btn dui-link dui-link--primary dui-util-txt-sm dui-util-spc-pad-0_8rem-xs dui-util-fw-bld"
+                            <a className="dui-nav-sch-act__signup-btn dui-link dui-link--primary dui-util-txt-sm dui-util-spc-pad-0_8rem-xs dui-util-fw-bld"
                                 href="/pages/authpage/signup.html">
                                 Sign Up
                             </a>
 
                             {/* <!-- Login Button --> */}
-                            <a class="dui-nav-sch-act__login-btn dui-link dui-link--primary dui-util-txt-sm dui-util-spc-pad-0_8rem-xs dui-util-fw-bld"
+                            <a className="dui-nav-sch-act__login-btn dui-link dui-link--primary dui-util-txt-sm dui-util-spc-pad-0_8rem-xs dui-util-fw-bld"
                                 href="/pages/authpage/login.html">
                                 Login
                             </a>
@@ -44,15 +44,15 @@ export default function Navbar() {
                     </li>
 
                     <li>
-                        <a class="dui-nav-sch-act__wish-lst-btn dui-util-disp-inline-block"
+                        <a className="dui-nav-sch-act__wish-lst-btn dui-util-disp-inline-block"
                             href="/pages/wishlistPage/wishlistPage.html">
-                            <div class="dui-badge dui-badge--round-ele-0_5-top-0_5-right">
+                            <div className="dui-badge dui-badge--round-ele-0_5-top-0_5-right">
                                 <div>
                                     0
                                 </div>
                             </div>
 
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart"
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-heart"
                                 width="56" height="56" viewBox="0 0 24 24" stroke-width="1.5" stroke="#9A9A9A"
                                 fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -63,10 +63,10 @@ export default function Navbar() {
                     </li>
 
                     <li>
-                        <a class="dui-nav-sch-act__cart_lst-btn dui-link" href="/pages/cartPage/cartPage.html">
-                            <div class="dui-util-clr-prim-p2">
+                        <a className="dui-nav-sch-act__cart_lst-btn dui-link" href="/pages/cartPage/cartPage.html">
+                            <div className="dui-util-clr-prim-p2">
                                 <svg xmlns="http://www.w3.org/2000/svg"
-                                    class="btn__icon icon icon-tabler icon-tabler-shopping-cart" width="56" height="56"
+                                    className="btn__icon icon icon-tabler icon-tabler-shopping-cart" width="56" height="56"
                                     viewBox="0 0 24 24" stroke-width="1.5" stroke="#9A9A9A" fill="none"
                                     stroke-linecap="round" stroke-linejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -75,16 +75,16 @@ export default function Navbar() {
                                     <path d="M17 17h-11v-14h-2" />
                                     <path d="M6 5l14 1l-1 7h-13" />
                                 </svg>
-                                <p class="dui-util-fw-bld dui-primary-color-p2">Cart</p>
+                                <p className="dui-util-fw-bld dui-primary-color-p2">Cart</p>
                             </div>
                         </a>
                     </li>
 
 
                     <li>
-                        <button class="dui-nav-sch-act__drk-mode-btn dui-btn reset-button-inherit-parent">
+                        <button className="dui-nav-sch-act__drk-mode-btn dui-btn reset-button-inherit-parent">
                             <svg xmlns="http://www.w3.org/2000/svg"
-                                class="dui-nav-sch-act__drk-mode-lgt icon icon-tabler icon-tabler-brightness-2"
+                                className="dui-nav-sch-act__drk-mode-lgt icon icon-tabler icon-tabler-brightness-2"
                                 width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff"
                                 fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -94,7 +94,7 @@ export default function Navbar() {
                             </svg>
 
                             <svg xmlns="http://www.w3.org/2000/svg"
-                                class="dui-nav-sch-act__drk-mode-drk dui-util-disp-none icon icon-tabler icon-tabler-moon"
+                                className="dui-nav-sch-act__drk-mode-drk dui-util-disp-none icon icon-tabler icon-tabler-moon"
                                 width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff"
                                 fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
