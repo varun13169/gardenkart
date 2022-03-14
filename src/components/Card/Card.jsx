3fcb1c96ba31fae06b311a57dbbf4a0011215f81
Varun Kumar <varun@Varun-MacBook-Pro.local>
2022-03-15 02:01:30 +0530

    Add Wishlist Button to Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -83,6 +83,25 @@ export default function Card(props) {
           </button>
         </div>
       </div>
+
+      {/* <!-- Button Component Starts -- Icon --> */}
+      <button class="main-content__wish-list-card-btn dui-btn dui-util-bdr-radi-999px-mx reset-button-inherit-parent">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="dui-util-spc-pad-0_8rem-xs icon icon-tabler icon-tabler-heart"
+          width="56"
+          height="56"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          fill="#F34E4E"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+        </svg>
+      </button>
+      {/* <!-- Button Component Ends -- Icon --> */}
     </div>
   );
 }
