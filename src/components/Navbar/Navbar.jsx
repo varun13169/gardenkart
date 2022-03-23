cc39489946c8b00703ca7296c589515439bf8d01
Varun Kumar <varun@Varun-MacBook-Pro.local>
2022-03-24 02:52:52 +0530

    Refactor Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router";
 import { useAuth } from "../../contexts";
+import {
+  MoonSVG,
+  SunSVG,
+  SearchSVG,
+  ShoppingCartSVG,
+} from "../../assets/svgReactComponents";
 
 export default function Navbar() {
   const { auth, checkValidTokenAndSetAuth } = useAuth();
@@ -19,22 +25,7 @@ export default function Navbar() {
       {/* <!-- Search --> */}
       <div className="dui-nav-sch-act__search dui-inp-txt dui-inp-txt--search">
         <i className="dui-inp-txt__icon dui-inp-txt__icon--search">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="dui-inp-txt__icon-content dui-inp-txt__icon-content--search icon icon-tabler icon-tabler-search"
-            width="56"
-            height="56"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="#059669"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <circle cx="10" cy="10" r="7" />
-            <line x1="21" y1="21" x2="15" y2="15" />
-          </svg>
+          <SearchSVG></SearchSVG>
         </i>
         <label>
           <input
@@ -100,11 +91,11 @@ export default function Navbar() {
               width="56"
               height="56"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="#9A9A9A"
               fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
@@ -115,24 +106,7 @@ export default function Navbar() {
         <li>
           <Link className="dui-nav-sch-act__cart_lst-btn dui-link" to="/cart">
             <div className="dui-util-clr-prim-p2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="btn__icon icon icon-tabler icon-tabler-shopping-cart"
-                width="56"
-                height="56"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="#9A9A9A"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <circle cx="6" cy="19" r="2" />
-                <circle cx="17" cy="19" r="2" />
-                <path d="M17 17h-11v-14h-2" />
-                <path d="M6 5l14 1l-1 7h-13" />
-              </svg>
+              <ShoppingCartSVG />
               <p className="dui-util-fw-bld dui-primary-color-p2">Cart</p>
             </div>
           </Link>
@@ -140,38 +114,9 @@ export default function Navbar() {
 
         <li>
           <button className="dui-nav-sch-act__drk-mode-btn dui-btn reset-button-inherit-parent">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="dui-nav-sch-act__drk-mode-lgt icon icon-tabler icon-tabler-brightness-2"
-              width="44"
-              height="44"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="#ffffff"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <circle cx="12" cy="12" r="3" />
-              <path d="M6 6h3.5l2.5 -2.5l2.5 2.5h3.5v3.5l2.5 2.5l-2.5 2.5v3.5h-3.5l-2.5 2.5l-2.5 -2.5h-3.5v-3.5l-2.5 -2.5l2.5 -2.5z" />
-            </svg>
+            {true && <SunSVG></SunSVG>}
 
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="dui-nav-sch-act__drk-mode-drk dui-util-disp-none icon icon-tabler icon-tabler-moon"
-              width="44"
-              height="44"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="#ffffff"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
-            </svg>
+            {false && <MoonSVG></MoonSVG>}
           </button>
         </li>
       </ul>
