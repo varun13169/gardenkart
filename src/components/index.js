5516b616783e750319238d6dbcd0371fd2521d96
Varun Kumar <varun@Varun-MacBook-Pro.local>
2022-05-18 03:10:14 +0530

    Fix Auth

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar/Navbar";
 import Card from "./Card/Card";
 import Sidebar from "./Sidebar/Sidebar";
+import RequiresAuth from "./RequiresAuth/RequiresAuth";
 
-export { Card, Sidebar, Navbar };
+export { Card, Sidebar, Navbar, RequiresAuth };
